67813f6aa4f3c45496e190db2b69719b8732977f
jerichox-uc <jerichox-uc@users.noreply.github.com>
2020-06-10 14:08:58 -0400

    avance nuxt

---
+++ pages/events.vue
@@ -0,0 +1,6 @@
+<template>
+<v-layout column justify-center align-center>
+  <h1>EVENTOS</h1>
+  <v-btn flat nuxt class="mx-2" to="/inspire">inspirate</v-btn>
+ </v-layout>
+</template>
---
+++ pages/list.vue
@@ -0,0 +1,5 @@
+<template>
+<v-layout column justify-center align-center>
+  <h1>LISTA</h1>
+ </v-layout>
+</template>
